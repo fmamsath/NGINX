648cdefa7edd87782fd93abbd0e3fbb677f68a4f
mohammed Amsath <111892924+fmamsath@users.noreply.github.com>
2025-08-09 07:45:06 +0300

    Added local_applicaion project

---
+++ local_applicaion/server/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("I am a endpoint");
+});
+
+app.listen(7777, () => {
+    console.log("this is working in port 7777")
+});
